d15d0a87dee7f9069aa51a2ffd22ff87ebf933d5
ERP Champions <devops@erpchampions.com>
2023-05-21 01:56:11 +0300

    auto add item/standard price for new room

--- havenir_hotel_erpnext/havenir_hotel_erpnext/doctype/rooms/rooms.py
+++ havenir_hotel_erpnext/havenir_hotel_erpnext/doctype/rooms/rooms.py
@@ -3,8 +3,29 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Rooms(Document):
-	pass
+    def after_insert(self):
+        try:
+            # Check if an item with the same item_code already exists
+            existing_item = frappe.get_value("Item", {"item_code": self.room_number})
+
+            if existing_item:
+                frappe.msgprint(f"Item with code {self.room_number} already exists.")
+            else:
+                item = frappe.new_doc("Item")
+                item.item_code = self.room_number
+                item.item_group = "Services"
+                item.item_name = self.room_name
+                item.standard_rate = self.price
+                item.is_stock_item = False
+                item.include_item_in_manufacturing = False
+                item.save()
+                
+                # Display a success message in the UI.
+                frappe.msgprint(f"Item {self.room_name} has been created successfully!")
+        except Exception as e:
+            # If there's an error, display the error message in the UI.
+            frappe.msgprint(f"An error occurred: {str(e)}")
